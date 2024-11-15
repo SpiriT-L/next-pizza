5e9bc9ad8d0906139f9886431c69f262f91abd7e
LeonID <lmatskevich79@gmail.com>
2024-11-15 23:38:22 +0300

    feat: add top bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import { Categories, Container, Title } from '@/components/shared';
+import { Container, Title } from '@/components/shared';
+import { TopBar } from '@/components/shared/top-bar';
 
 export default function Home() {
   return (
     <>
       <Container className='mt-10'>
         <Title text={'Все пиццы'} size='lg' className='font-extrabold' />
-        <Categories />
       </Container>
+      <TopBar />
+      <div className='h-[3000px]'></div>
     </>
   );
-}
+} 
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -2,4 +2,4 @@ export {Header} from './header';
 export {Container} from './container';
 export {Title} from './title';
 export {Categories} from './categories';
-export {SortPopup} from './sortPopup';
+export {SortPopup} from './sort-popup';
