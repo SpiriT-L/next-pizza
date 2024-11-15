32a0ffd01b785f7a1d9e94299a7be971b4076222
LeonID <lmatskevich79@gmail.com>
2024-11-16 00:27:29 +0300

    feat: add checkbox

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Title } from '@/components/shared';
+import { Container, Filters, Title } from '@/components/shared';
 import { TopBar } from '@/components/shared/top-bar';
 
 export default function Home() {
@@ -8,7 +8,16 @@ export default function Home() {
         <Title text={'Все пиццы'} size='lg' className='font-extrabold' />
       </Container>
       <TopBar />
-      <div className='h-[3000px]'></div>
+      <Container className='pb-14 mt-10'>
+        <div className='flex gap-[60px]'>
+          <div className='w-[250px]'>
+            <Filters />
+          </div>
+          <div className='flex-1'>
+            <div className='flex flex-col gap-16'>Список товаров</div>
+          </div>
+        </div>
+      </Container>
     </>
   );
-} 
+}
