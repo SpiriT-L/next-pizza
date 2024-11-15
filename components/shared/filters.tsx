1ef7850243647eff85adac312f8a68e4f067cb60
LeonID <lmatskevich79@gmail.com>
2024-11-16 00:38:24 +0300

    feat: add input price

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { FilterCheckbox } from './filter-checkbox';
 import { Title } from './title';
-import { FilterCheckbox } from './filter-checkbox'
+import { Input } from '../ui'
 
 interface Props {
   className?: string;
@@ -8,15 +9,19 @@ interface Props {
 
 export const Filters: React.FC<Props> = ({ className }) => {
   return (
-    <>
-      <div className={className}>
-        <Title text='Фильтрация' size='sm' className='mb-5 font-bold' />
-      </div>
-
+    <div className={className}>
+      <Title text='Фильтрация' size='sm' className='mb-5 font-bold' />
       <div className='flex flex-col gap-4'>
         <FilterCheckbox text='Можно собрать' value='1' />
         <FilterCheckbox text='Новинки' value='2' />
       </div>
-    </>
+      <div className='mt-5 border-y border-y-neutral-100 py-6 pb-7'>
+        <p>Цена от и до:</p>
+        <div className='flex gap-3 mb-5'>
+          <Input type='number' placeholder='0' min={0} max={100} defaultValue={0} />
+          <Input type='number' placeholder='10' min={10} max={100} />
+        </div>
+      </div>
+    </div>
   );
 };
